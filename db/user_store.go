1e42d66c1ee68336db96430bd4568e5972ab0f0e
fayob <adedejifavour6@gmail.com>
2023-07-11 17:10:14 +0100

    Add db user store

---
+++ db/user_store.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"hotel_reservation/types"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	DBNAME = "hotel_reservation"
+	userCollection = "users"
+)
+
+type UserStore interface {
+	GetUserByID(context.Context, string) (*types.User, error)
+}
+
+type MongoUserStore struct {
+	client     *mongo.Client
+	collection *mongo.Collection
+}
+
+func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
+	return &MongoUserStore{
+		client: client,
+		collection: client.Database(DBNAME).Collection(userCollection),
+	}
+}
+
+func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	var user types.User
+	if err := s.collection.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
