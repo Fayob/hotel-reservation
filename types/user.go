391a8ff9fbf179693ea9a89d955b83808b677bdc
fayob <adedejifavour6@gmail.com>
2023-07-12 11:38:42 +0100

    Add more type and structs

--- types/user.go
+++ types/user.go
@@ -1,7 +1,85 @@
 package types
 
+import (
+	"fmt"
+	"regexp"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	bcryptCost = 12
+	minFirstNameLen = 2
+	minLastNameLen = 2
+	minPasswordLen = 7
+)
+
+type UpdateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func (p UpdateUserParams) ToBSON() bson.M {
+	m := bson.M{}
+	if len(p.FirstName) > 1 {
+		m["firstName"] = p.FirstName
+	}
+	if len(p.LastName) > 1 {
+		m["lastName"] = p.LastName
+	}
+	return m
+}
+
+type CreateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+func (params CreateUserParams) Validate() map[string]string {
+	errors := map[string]string{}
+	if len(params.FirstName) < minFirstNameLen {
+		errors["firstName"] = fmt.Sprintf("first name length should be at least %d characters", minFirstNameLen)
+	}
+	if len(params.LastName) < minLastNameLen {
+		errors["lastName"] = fmt.Sprintf("last name length should be at least %d characters", minLastNameLen)
+	}
+	if len(params.Password) < minPasswordLen {
+		errors["password"] = fmt.Sprintf("password length should be at least %d characters", minPasswordLen)
+	}
+	if !isEmailValid(params.Email) {
+		errors["email"] = fmt.Sprintf("%v is an invalid email", params.Email)
+	}
+	return errors
+}
+
+func isEmailValid(e string) bool {
+	// _, err := mail.ParseAddress(e)
+	// return err == nil
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(e)
+}
+
 type User struct {
-	ID        string `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName string `bson:"firstName" json:"firstName"`
-	LastName  string `bson:"lastName" json:"lastName"`
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName         string `bson:"firstName" json:"firstName"`
+	LastName          string `bson:"lastName" json:"lastName"`
+	Email             string `bson:"email" json:"email"`
+	EncryptedPassword string `bson:"encryptedPassword" json:"-"`
+}
+
+func NewUserFromParams(params CreateUserParams) (*User, error) {
+	encrytpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		FirstName: params.FirstName,
+		LastName: params.LastName,
+		Email: params.Email,
+		EncryptedPassword: string(encrytpw),
+	}, nil
 }
