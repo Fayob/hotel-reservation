ac5af27987d71a109e5de6637c664ecbe1885840
fayob <adedejifavour6@gmail.com>
2023-07-13 13:48:00 +0100

    Add store struct to db

--- db/db.go
+++ db/db.go
@@ -1,1 +1,13 @@
 package db
+
+const (
+	DBURI      = "mongodb://localhost:27017"
+	DBNAME     = "hotel_reservation"
+	TESTDBNAME = "hotel_reservation_test"
+)
+
+type Store struct {
+	User  UserStore
+	Hotel HotelStore
+	Room  RoomStore
+}
