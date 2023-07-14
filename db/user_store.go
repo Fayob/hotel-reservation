fa650eaf746add9c9a0bb53e8f6f00584889fe0f
fayob <adedejifavour6@gmail.com>
2023-07-14 18:13:17 +0100

    Add new method to get user by email

--- db/user_store.go
+++ db/user_store.go
@@ -22,6 +22,7 @@ type UserStore interface {
 	Dropper
 
 	GetUserByID(context.Context, string) (*types.User, error)
+	GetUserByEmail(context.Context, string) (*types.User, error)
 	GetUsers(context.Context) ([]*types.User, error)
 	CreateUser(context.Context, *types.User) (*types.User, error)
 	DeleteUser(context.Context, string)  error
@@ -43,6 +44,7 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 func (s *MongoUserStore) Drop(ctx context.Context) error {
 	return s.collection.Drop(ctx)
 }
+
 func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
 	oid, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
@@ -50,6 +52,13 @@ func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.Use
 	}
 	var user types.User
 	if err := s.collection.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+func (s *MongoUserStore) GetUserByEmail(ctx context.Context, email string) (*types.User, error) {
+	var user types.User
+	if err := s.collection.FindOne(ctx, bson.M{"email": email}).Decode(&user); err != nil {
 		return nil, err
 	}
 	return &user, nil
